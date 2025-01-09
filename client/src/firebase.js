348b0a38e43bcde33be23c7f5e8dac8705d8eef3
Amit dey <101701030+Amit00008@users.noreply.github.com>
2025-01-09 15:21:36 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEBNZ3RmT0gG7F0oYaxIQ17jiWAbM0eiI",
-  authDomain: "pumpfun-93b82.firebaseapp.com",
-  projectId: "pumpfun-93b82",
-  storageBucket: "pumpfun-93b82.firebasestorage.app",
-  messagingSenderId: "850424682793",
-  appId: "1:850424682793:web:5ef54df02819640115ff38",
-  measurementId: "G-YK1NJWNM6M"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
